55055bc46fc930d755e1762b2fb8cd9be71cfe26
Jiefeng Ou <jou60@stuy.edu>
2025-12-04 10:57:07 -0500

    Added working flask app

--- app/__init__.py
+++ app/__init__.py
@@ -2,3 +2,14 @@
 # monky
 # SoftDev
 # P01: ArRESTed Development
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("home.html");
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
