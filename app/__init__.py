b8637310e73880ecd77482db11b0c33a14c2f6fe
Jiefeng Ou <jou60@stuy.edu>
2025-12-05 10:37:00 -0500

    Added sqlite tables

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,67 @@
 # P01: ArRESTed Development
 
 from flask import Flask, render_template
+import sqlite3
+
 app = Flask(__name__)
+
+DB_FILE="database.db"
+db = sqlite3.connect(DB_FILE, check_same_thread=False)
+c = db.cursor()
 
 @app.route("/")
 def hello_world():
-    return render_template("home.html");
+    return render_template("home.html")
 
-if __name__ == "__main__":
-    app.debug = True
+#==========================================================
+#SQLITE3 DATABASE LIES BENEATH HERE
+#==========================================================
+
+#users (username, password)
+c.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    username TEXT PRIMARY KEY,
+    password TEXT
+)""")
+
+#poke_stats (username, wins, last_daily, daily_streak)
+c.execute("""
+CREATE TABLE IF NOT EXISTS poke_stats (
+    username TEXT,
+    wins INTEGER,
+    last_daily DATE,
+    daily_streak INTEGER,
+    FOREIGN KEY (username) REFERENCES users(username)
+)""")
+
+#cat_stats (username, wins, last_daily, daily_streak)
+c.execute("""
+CREATE TABLE IF NOT EXISTS cat_stats (
+    username TEXT,
+    wins INTEGER,
+    last_daily DATE,
+    daily_streak INTEGER,
+    FOREIGN KEY (username) REFERENCES users(username)
+)""")
+
+#bird_stats (username, wins, last_daily, daily_streak)
+c.execute("""
+CREATE TABLE IF NOT EXISTS bird_stats (
+    username TEXT,
+    wins INTEGER,
+    last_daily DATE,
+    daily_streak INTEGER,
+    FOREIGN KEY (username) REFERENCES users(username)
+)""")
+
+#==========================================================
+#SQLITE3 DATABASE LIES ABOVE HERE
+#==========================================================
+
+db.commit() #save changes
+
+if __name__ == "__main__": #false if this file imported as module
+    app.debug = True  #enable PSOD, auto-server-restart on code chg
     app.run()
+
+db.close()  #close database
